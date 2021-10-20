8c85b51abef74e9b18e657484d504abb77ebd412
Mochamad Saepul Millah <numusai@gmail.com>
2021-10-20 15:55:18 +0700

    Initialize project

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => { res.send('Working As')})
+
+app.listen(port, () => {
+  console.log("Listening on port : " + port);
+});
---
+++ migrations/20211020083845-create-chat-room.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('ChatRooms', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      MovieId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Movies"
+        }
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users"
+        }
+      },
+      message: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('ChatRooms');
+  }
+};
