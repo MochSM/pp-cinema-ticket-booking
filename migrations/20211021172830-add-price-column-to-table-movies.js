71b390aa4f7dc03896d9efddb082bbeeb309f17d
rizkybor <rizkyak994@gmail.com>
2021-10-22 00:49:32 +0700

    Update seeders, new migration, and json user

---
+++ migrations/20211021172830-add-price-column-to-table-movies.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn("Movies","price",{
+      type: Sequelize.INTEGER,
+      references: {
+        model: "Movies",
+      },
+    })
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn("Movies","price",{})
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
