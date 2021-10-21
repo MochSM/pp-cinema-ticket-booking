41afff92ddb28e529eb5962e2233d00a25efaba5
rizkybor <rizkyak994@gmail.com>
2021-10-21 18:16:12 +0700

    update controllers

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -10,22 +10,27 @@ class movieController {
 
     
     static createFormMovie(req, res){
-        
+        res.render('formAddMovie', {loggedIn:false})
     }
     
-    static updateFormMovie(req, res){
-        
+    static postCreateFormMovie(req, res){
+        // res.send('masuuukk')
+        // let newMovies = {
+        //     title: req.body.title
+        //     posterURL: req.body.
+        // }
     }
     
     static readMovieById(req,res){
-
+        let id = req.params.movieId
+        Movie.findByPk(id)
     }
     
     static editFormMovie(req, res){
 
     }
 
-    static updateFormMovie(req,res){
+    static postEditFormMovie(req,res){
 
     }
 
