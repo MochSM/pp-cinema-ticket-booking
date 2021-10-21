c9fbd268e29acf44439604174f21a13018550c1e
Mochamad Saepul Millah <numusai@gmail.com>
2021-10-22 04:12:40 +0700

    Add chat box

--- app.js
+++ app.js
@@ -1,27 +1,84 @@
 const express = require("express");
-const router = require('./routers');
-const sessions = require('express-session');
-const HomeController = require('./controllers/homeController')
+const router = require("./routers");
+const sessions = require("express-session");
+const HomeController = require("./controllers/homeController");
+
+// requirement for socketio
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const socketPort = 8000;
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 // view engine
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
+
+// cookie parser middleware
+app.use(cookieParser());
+
+const { emit } = require("process"); // SEEMS NOT USED
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "MAIN_URL",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
 
 //session middleware
-app.use(sessions({
+app.use(
+  sessions({
     secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
-    saveUninitialized:true,
+    saveUninitialized: true,
     cookie: { maxAge: 1000 * 60 * 60 * 24 },
-    resave: false
-}));
+    resave: false,
+  })
+);
 
 // parsing middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', HomeController.readMovie );
-app.use('/', router)
+app.get("/", HomeController.readMovie);
+app.use("/", router);
+
+const ChatRoomController = require("./controllers/chatRoomController");
+
+// sends out the 10 most recent messages from recent to old
+const emitMostRecentMessges = () => {
+  ChatRoomController.getSocketMessages()
+    .then((result) => {
+      io.emit("chat message", result);
+    })
+    .catch(console.log);
+};
+
+// connects, creates message, and emits top 10 messages
+io.on("connection", (socket) => {
+  console.log("a user connectedddd");
+  // socket.on(event) event is string like "chat message"
+
+  socket.on("chat message", (msg) => {
+    console.log("masuk controller", msg);
+    ChatRoomController.createSocketMessage(JSON.parse(msg))
+      .then((_) => {
+        emitMostRecentMessges();
+      })
+      .catch((err) => io.emit(err));
+  });
+
+  // close event when user disconnects from app
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+// Displays in terminal which port the socketPort is running on
+server.listen(socketPort, () => {
+  console.log(`listening on *:${socketPort}`);
+});
 
 app.listen(port, () => console.log(`App running on port ${port}.`));
--- controllers/chatRoomController.js
+++ controllers/chatRoomController.js
@@ -2,36 +2,36 @@ const { ChatRoom } = require("../models/");
 
 class ChatRoomController {
   static getMessages(req, res) {
-    ChatRoom.findAll({ include: 'User' })
+    ChatRoom.findAll({ include: "User" })
       .then((messages) => res.send(messages))
       .catch((err) => res.send(err));
   }
-  // ???
   
+  static createMessage(req, res) {
+    const { text, username } = request.body;
+    const params = { text, username };
+    ChatRoom.create(params)
+      .then((messages) => res.send(messages))
+      .catch((err) => res.send(err));
+  }
+
   static getSocketMessages() {
     return new Promise((resolve, reject) => {
-      ChatRoom.findAll({ include: 'User' })
-        .then((messages) => {
-          console.log(messages, "<<<<< included");
-          resolve(messages)
-        })
-        .catch((err) => {
-          console.log(err, "<<<<< included");
-          reject(err)
-        });
+      ChatRoom.findAll({ include: "User" })
+        .then((messages) => resolve(messages))
+        .catch((err) => reject(err));
     });
- };
+  }
 
- static createSocketMessage(messageObj) {
-  const { message, UserId } = messageObj;
-  const params = { message, UserId, MovieId: 1};
-  return new Promise((resolve) => {
-    ChatRoom.create(params)
-      .then((data) => resolve(data.dataValues))
-      .catch((err) => res.send(err));
-    })
-  };
-
+  static createSocketMessage(messageObj) {
+    const { message, UserId } = messageObj;
+    const params = { message, UserId, MovieId: 1 };
+    return new Promise((resolve) => {
+      ChatRoom.create(params)
+        .then((data) => resolve(data.dataValues))
+        .catch((err) => res.send(err));
+    });
+  }
 }
 
 module.exports = ChatRoomController;
