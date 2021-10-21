e619d46cf3cf1b1affa3a79fad23c95634db41f4
Mochamad Saepul Millah <numusai@gmail.com>
2021-10-21 23:20:24 +0700

    add association to movie

--- models/movie.js
+++ models/movie.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Movie.hasMany(models.Ticket)
     }
     
     get formatDate(){
