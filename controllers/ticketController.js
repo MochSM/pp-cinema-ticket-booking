8410677de3b0c3149ba4aef82e6aea0a481ea4bb
Mochamad Saepul Millah <numusai@gmail.com>
2021-10-21 23:50:16 +0700

    Add buy ticket

---
+++ controllers/ticketController.js
@@ -0,0 +1,69 @@
+const { Ticket, Movie } = require("../models/");
+const formatMonetary = require("../helpers/monetaryFormatter")
+const { formatDate, formatTime} = require("../helpers/dateFormatter")
+
+class TicketController {
+  static listTickets(req, res) {
+    const params = { UserId: req.session.userId }
+    const { message } = req.query
+    Ticket.findAll({ where: params, include: 'Movie'})
+      .then((data) => res.render("ticket/listTicket", { data, message, loggedIn:req.session.userId, user:req.session, formatMonetary, formatDate, formatTime }))
+      .catch((err) => res.send(err));
+  }
+
+  static getTicketAddForm(req, res) {
+    const { MovieId } = req.params;
+    const params = { id:MovieId }
+    const { message, error } = req.query;
+
+    Movie.findOne({ where: params, include: { 
+      model: Ticket,
+      require: false
+    }})
+      .then((data) => {
+        const seatRow = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+        const bookedSeat = []
+        for (const rowCode of seatRow) {
+          let temp = []
+          for (let i = 1; i <= 8; i++) {
+            let flag = false
+            data.Tickets.forEach(seat => {
+              if(rowCode == seat.seatNumber[0] && i == seat.seatNumber[1]) {
+                temp.push(seat.seatNumber)
+                flag = true;
+              }
+            })
+            if(!flag) temp.push(null)
+          }
+          bookedSeat.push(temp)
+        }
+        res.render("ticket/buyTicket", { data, message, error, bookedSeat, loggedIn:req.session.userId, user:req.session, });
+      })
+      .catch((err) => res.send(err));
+  }
+
+  static postTicketAddForm(req, res) {
+    const { UserId, seatNumber, date, time } = req.body;
+    const { MovieId } = req.params;
+    const dateParams = (date && time) ? `${date.split('-')},${time.split(':')}`.split(",") : ''
+    const showTime = dateParams ? new Date(dateParams[2], dateParams[1], dateParams[0], dateParams[3], dateParams[4]) : ''
+    const params = { UserId, MovieId, seatNumber: seatNumber ? seatNumber : '', price: 50000, showTime }
+    Ticket.create(params)
+      .then(() => res.redirect("/tickets"))
+      .catch((err) => res.redirect(`/tickets/buyticket/${MovieId}?error=${err.message.split('\n').map(el => el.replace("Validation error: ", "")).join(".")}`));
+  }
+
+  static destroyTicket(req, res) {
+    const { ticketId } = req.params
+    const params = { id: ticketId, UserId: req.session.userId }
+    Ticket.findOne({ where: params })
+      .then((data) => {
+        const message = `Ticket with number ${data.ticketNumber} was canceled!`
+        Ticket.destroy({ where: params })
+        .then(() => res.redirect(`/tickets/?message=${message}`))
+      })
+      .catch((err) => res.send(err));
+  }
+}
+
+module.exports = TicketController;
