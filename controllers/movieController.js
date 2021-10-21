b3efb6217b9aabf64af651aa706431d392a21e2e
rizkybor <rizkyak994@gmail.com>
2021-10-21 21:56:08 +0700

    update edit controllers & getter date di model movie

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -4,30 +4,37 @@ const {Movie} = require("../models")
 class movieController {
     static readMovie(req,res){
         Movie.findAll()
-        .then((data)=>{res.send(data)})
+        .then((data)=>{res.render('showMovies', {data, loggedIn:false})})
         .catch((err)=>{res.send(err.message)})
     }
 
-    
     static createFormMovie(req, res){
         res.render('formAddMovie', {loggedIn:false})
     }
     
     static postCreateFormMovie(req, res){
-        // res.send('masuuukk')
-        // let newMovies = {
-        //     title: req.body.title
-        //     posterURL: req.body.
-        // }
-    }
-    
-    static readMovieById(req,res){
-        let id = req.params.movieId
-        Movie.findByPk(id)
+        console.log (req.body)
+        let newMovies = {
+            title: req.body.title,
+            posterUrl: req.body.posterURL,
+            pgRating: req.body.pgRating,
+            genre: req.body.genre,
+            synopsis: req.body.synopsis,
+            releaseDate: req.body.date,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        };
+
+        Movie.create(newMovies)
+        .then((data) => {res.redirect('showMovies')})
+        .catch((err) => {res.send(err)})
     }
     
     static editFormMovie(req, res){
-
+        let id = req.params.movieId
+        Movie.findByPk(id)
+        .then((data) => {res.render('formEditMovies', {data, loggedIn:false})})
+        .catch((err)=> res.send(err))
     }
 
     static postEditFormMovie(req,res){
@@ -35,7 +42,11 @@ class movieController {
     }
 
     static destroyMovie(req,res){
-
+        // let id = req.params.movieId
+        // console.log(id)
+        // Movie.destroyMovie({ where: { id: id}})
+        //  .then((data) => {res.redirect()})
+        //  .catch((err)=>{res.send(err)})
     }
 
 }
